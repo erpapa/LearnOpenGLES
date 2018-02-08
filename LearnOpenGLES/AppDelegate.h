8d328bd28869b4580e5ccedfe7e8236aa6a34ab2
erpapa <erpapa@erpapademacbook-pro.local>
2018-02-08 18:55:06 +0800

    Initial Commit

---
+++ LearnOpenGLES/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnOpenGLES
+//
+//  Created by erpapa on 2018/2/8.
+//  Copyright © 2018年 erpapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
